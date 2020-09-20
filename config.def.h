cef1195b1f4b90fd5d29ab84368b9efdc3129dd3
Jackson McCrory <jackson@jacksonmccrory.com>
2020-09-20 21:57:18 +0000

    Major changes

--- config.def.h
+++ config.def.h
@@ -1,4 +1,5 @@
 /* See LICENSE file for copyright and license details. */
+#include <X11/XF86keysym.h>
 
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
@@ -6,13 +7,13 @@ static const unsigned int gappx     = 4;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Fira Code Nerd Font:size=10" };
-static const char dmenufont[]       = "Fira Code Nerd Font:size=10";
+static const char *fonts[]          = { "Sauce Code Pro Nerd Font:size=10" };
+static const char dmenufont[]       = "Sauce Code Pro Nerd Font:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#750b09";
+static const char col_cyan[]        = "#cc1111";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -61,7 +62,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -69,11 +70,17 @@ static Key keys[] = {
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_w,      spawn,          CMD("firefox") },
-	{ MODKEY,                       XK_r,      spawn,          CMD("st -e ranger") },
+	{ MODKEY,                       XK_m,      spawn,          CMD("thunderbird-bin") },
+	{ MODKEY,                       XK_r,      spawn,          CMD("alacritty -e ranger") },
+	{ MODKEY|ShiftMask,             XK_l,      spawn,          CMD("slock") },
+	{ 0,                            XF86XK_AudioLowerVolume,      spawn,          CMD("pulsemixer --change-volume -1") },
+	{ 0,                            XF86XK_AudioRaiseVolume,      spawn,          CMD("pulsemixer --change-volume +1 && pulsemixer --max-volume 100") },
+	{ 0,                            XF86XK_AudioMicMute,          spawn,          CMD("pulsemixer --toggle-mute --id source-2") },
+	{ 0,                            XF86XK_AudioMute,             spawn,          CMD("pulsemixer --toggle-mute") },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY|ShiftMask,                       XK_j,      rotatestack,     {.i = +1 } },
-	{ MODKEY|ShiftMask,                       XK_k,      rotatestack,     {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_j,      rotatestack,    {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_k,      rotatestack,    {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
@@ -83,7 +90,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY|ShiftMask,             XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
@@ -120,4 +127,3 @@ static Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
-
