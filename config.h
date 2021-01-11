19b84ab9aabf54af4fa3c05612e48b57350418de
Jackson McCrory <jackson@mccrory.xyz>
2021-01-10 18:51:00 -0800

    Switched to one dark settings and added the status2d patch

--- config.h
+++ config.h
@@ -14,15 +14,17 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Sauce Code Pro Nerd Font:size=10" };
 static const char dmenufont[]       = "Sauce Code Pro Nerd Font:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#507ab2";
+static const char col_gray1[]       = "#282c34";
+static const char col_gray2[]       = "#353b45";
+static const char col_gray3[]       = "#3e4451";
+static const char col_white[]       = "#abb2bf";
+static const char col_blue[]        = "#61afef";
+static const char col_red[]         = "#e06c75";
+static const char col_yellow[]      = "#e5c07b";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_white, col_gray1, col_gray3 },
+	[SchemeSel]  = { col_white, col_gray2,  col_blue  },
 };
 
 /* tagging */
@@ -71,7 +73,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_white, "-sb", col_blue, "-sf", col_white, NULL };
 static const char *termcmd[]  = { "alacritty", "-e", "fish", NULL };
 
 static Key keys[] = {
@@ -79,13 +81,24 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_w,      spawn,          CMD("firefox") },
-	{ MODKEY,                       XK_m,      spawn,          CMD("thunderbird-bin") },
+	{ MODKEY,                       XK_w,      spawn,          CMD("brave") },
 	{ MODKEY|ShiftMask,             XK_l,      spawn,          CMD("slock") },
+	{ MODKEY|ShiftMask,             XK_s,      spawn,          CMD("spotify") },
+	{ MODKEY,                       XK_F6,     spawn,          CMD("~/.config/dmenu/scripts/dmenu-power.sh") },
+	{ MODKEY,                       XK_F7,     spawn,          CMD("~/.config/dmenu/scripts/dmenu-ssh.sh") },
+	{ MODKEY,                       XK_F8,     spawn,          CMD("~/.config/dmenu/scripts/dmenu-sysmon.sh") },
+	{ MODKEY,                       XK_F9,     spawn,          CMD("~/.config/dmenu/scripts/dmenumount.sh") },
+	{ MODKEY,                       XK_F10,    spawn,          CMD("~/.config/dmenu/scripts/dmenuumount.sh") },
+	{ MODKEY,                       XK_F11,    spawn,          CMD("~/.config/dmenu/scripts/dmenu-scrot.sh") },
+	{ MODKEY,                       XK_F12,    spawn,          CMD("~/.config/dmenu/scripts/dmenu-edit-configs.sh") },
 	{ 0,                            XF86XK_AudioLowerVolume,      spawn,          CMD("pulsemixer --change-volume -2") },
 	{ 0,                            XF86XK_AudioRaiseVolume,      spawn,          CMD("pulsemixer --change-volume +2 && pulsemixer --max-volume 100") },
 	{ 0,                            XF86XK_AudioMicMute,          spawn,          CMD("pulsemixer --toggle-mute --id source-2") },
 	{ 0,                            XF86XK_AudioMute,             spawn,          CMD("pulsemixer --toggle-mute") },
+	{ 0,                            XF86XK_AudioPlay,             spawn,          CMD("playerctl -p spotify play-pause") },
+	{ 0,                            XF86XK_AudioPrev,             spawn,          CMD("playerctl -p spotify previous") },
+	{ 0,                            XF86XK_AudioNext,             spawn,          CMD("playerctl -p spotify next") },
+	{ 0,                            XF86XK_AudioStop,             spawn,          CMD("playerctl -p spotify stop") },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_j,      rotatestack,    {.i = +1 } },
