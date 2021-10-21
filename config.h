b8021f6182f19de55789dd7cdbafc4275ecb0734
Jackson McCrory <jackson@mccrory.xyz>
2021-10-20 17:46:56 -0700

    new initial commit

--- config.h
+++ config.h
@@ -10,26 +10,26 @@
 /* appearance */
 static unsigned int borderpx = 2; /* border pixel of windows */
 static unsigned int snap = 32;    /* snap pixel */
-static unsigned int gappih = 10;  /* horiz inner gap between windows */
-static unsigned int gappiv = 10;  /* vert inner gap between windows */
+static unsigned int gappih = 16;  /* horiz inner gap between windows */
+static unsigned int gappiv = 16;  /* vert inner gap between windows */
 static unsigned int gappoh =
-    10; /* horiz outer gap between windows and screen edge */
+    16; /* horiz outer gap between windows and screen edge */
 static unsigned int gappov =
-    10; /* vert outer gap between windows and screen edge */
+    16; /* vert outer gap between windows and screen edge */
 static int swallowfloating =
     0; /* 1 means swallow floating windows by default */
 static int smartgaps =
     0;                  /* 1 means no outer gap when there is only one window */
 static int showbar = 1; /* 0 means no bar */
 static int topbar = 1;  /* 0 means bottom bar */
-static char *fonts[] = {"JetBrains Mono Nerd Font:size=10",
-                        "JoyPixels:pixelsize=10:antialias=true:autohint=true"};
-static const char dmenufont[] = "JetBrains Mono Nerd Font:size=10";
+static char *fonts[] = {"RobotoMono Nerd Font:size=10",
+                        "JoyPixels:pixelsize=11:antialias=true:autohint=true"};
+static const char dmenufont[] = "RobotoMono Nerd Font:size=10";
 
-static char vibrantcolor[] = "#23d18b";
-static char backgroundcolor[] = "#1e1e1e";
-static char inactivebordercolor[] = "#444444";
-static char foregroundcolor[] = "#d8dee9";
+static char vibrantcolor[] = "#ff9e64";
+static char backgroundcolor[] = "#1a1b26";
+static char inactivebordercolor[] = "#808080";
+static char foregroundcolor[] = "#c0caf5";
 
 static char *colors[][3] = {
     /*               fg           bg           border   */
@@ -72,6 +72,7 @@ static const Rule rules[] = {
     {NULL, "spspot", NULL, SPTAG(1), 1, 1, 0, -1},
     {NULL, "spcalc", NULL, SPTAG(2), 1, 1, 0, -1},
     {"Virt-manager", NULL, NULL, 0, 1, 0, 1, -1},
+    {"Nextcloud", NULL, NULL, 0, 1, 0, 1, -1},
     /* {"VirtualBox Manager", NULL, NULL, 0, 1, 0, 1, -1}, */
     /* {"VirtualBox Machine", NULL, NULL, 0, 1, 0, 1, -1}, */
     /* {"VirtualBoxVM", NULL, NULL, 0, 1, 0, 1, -1}, */
@@ -129,9 +130,10 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] =
     "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = {
-    "dmenu_run", "-m",  dmenumon,  "-fn", dmenufont,    "-nb", backgroundcolor, "-nf",
-    "#abb2bf",   "-sb", "#303030", "-sf", vibrantcolor, NULL};
+static const char *dmenucmd[] = {"dmenu_run",  "-m",  dmenumon,        "-fn",
+                                 dmenufont,    "-nb", backgroundcolor, "-nf",
+                                 "#abb2bf",    "-sb", "#303030",       "-sf",
+                                 vibrantcolor, NULL};
 static const char *termcmd[] = {TERMINAL, NULL};
 
 /*
@@ -170,8 +172,10 @@ static Key keys[] = {
                 TAGKEYS(XK_9, 8)
 
                     {MODKEY | ShiftMask, XK_Return, spawn, {.v = dmenucmd}},
+    {MODKEY | ShiftMask | ControlMask, XK_Return, spawn,
+     SHCMD("~/.config/rofi/launchers/colorful/launcher.sh")},
     {MODKEY, XK_Return, spawn, {.v = termcmd}},
-    {MODKEY, XK_w, spawn, SHCMD("$BROWSER")},
+    {MODKEY, XK_w, spawn, SHCMD("firefox")},
     {Mod1Mask | ControlMask, XK_e, spawn, SHCMD("emacsclient -c -n -a emacs")},
     {Mod1Mask | ControlMask, XK_b, spawn,
      SHCMD("emacsclient -c -n -a emacs --eval '(ibuffer)'")},
@@ -212,23 +216,31 @@ static Key keys[] = {
      SHCMD("pamixer -i 2 ; pkill -RTMIN+3 dwmblocks")},
     {0, XF86XK_AudioMute, spawn,
      SHCMD("pamixer --toggle-mute ; pkill -RTMIN+3 dwmblocks")},
-    {0, XF86XK_AudioPlay, spawn, SHCMD("playerctl -p spotify play-pause")},
-    {0, XF86XK_AudioPrev, spawn, SHCMD("playerctl -p spotify previous")},
-    {0, XF86XK_AudioNext, spawn, SHCMD("playerctl -p spotify next")},
-    {0, XF86XK_AudioStop, spawn, SHCMD("playerctl -p spotify stop")},
-    {MODKEY | ShiftMask, XK_q, spawn,
-     SHCMD("~/.config/dmenu/scripts/dmenu-prompt.sh 'Quit DWM?' 'killall "
-           "xinit'")},
+    {0, XF86XK_AudioPlay, spawn,
+     SHCMD("/home/jackson/.local/bin/music-control toggle")},
+    {0, XF86XK_AudioPrev, spawn,
+     SHCMD("/home/jackson/.local/bin/music-control prev")},
+    {0, XF86XK_AudioNext, spawn,
+     SHCMD("/home/jackson/.local/bin/music-control next")},
+    {0, XF86XK_AudioStop, spawn,
+     SHCMD("/home/jackson/.local/bin/music-control stop")},
+    {MODKEY | ShiftMask, XK_q, spawn, SHCMD("/home/jackson/.local/bin/sysact")},
     {MODKEY, XK_n, spawn,
      SHCMD(TERMINAL " -e newsboat; pkill -RTMIN+6 dwmblocks")},
-    {MODKEY, XK_t, spawn, SHCMD(TERMINAL " -e tremc")},
+    {MODKEY, XK_t, spawn, SHCMD(TERMINAL " -e stig")},
+    {MODKEY | ShiftMask, XK_t, spawn, SHCMD("starttorrent")},
+    {MODKEY, XK_m, spawn, SHCMD(TERMINAL " -e ncmpcpp")},
+    {MODKEY | ShiftMask, XK_m, spawn, SHCMD(TERMINAL " -e neomutt")},
     {MODKEY | ShiftMask, XK_v, spawn, SHCMD("togglevpn")},
     {MODKEY, XK_u, spawn, SHCMD(TERMINAL " -e update-system")},
     {MODKEY, XK_0, view, {.ui = ~0}},
     {MODKEY | ShiftMask, XK_0, tag, {.ui = ~0}},
     {MODKEY, XK_Tab, view, {0}},
     {MODKEY | ShiftMask, XK_c, killclient, {0}},
-    {MODKEY | ShiftMask, XK_t, setlayout, {.v = &layouts[0]}}, /* tile */
+    {MODKEY | Mod1Mask | ShiftMask,
+     XK_t,
+     setlayout,
+     {.v = &layouts[0]}}, /* tile */
     // {MODKEY | ShiftMask, XK_t, setlayout, {.v = &layouts[1]}}, /* bstack */
     /* { MODKEY,			        XK_u, setlayout,
        {.v = &layouts[4]} }, /1* deck *1/ */
@@ -266,6 +278,7 @@ static Key keys[] = {
     /* F Keys */
     {MODKEY, XK_F1, spawn,
      SHCMD("sxiv -r -q -t -o ~/.local/repos/wallpapers/*")},
+    {MODKEY, XK_F2, spawn, SHCMD("~/.local/bin/togglesystray")},
 
     {MODKEY, XK_z, incrgaps, {.i = +3}},
     {MODKEY, XK_x, incrgaps, {.i = -3}},
